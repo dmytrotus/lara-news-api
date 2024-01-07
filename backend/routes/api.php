55cf4f250f8b7343bfc6a43bfd9cc467d9527a5f
Dmytro Tus <dmytrotus@gmail.com>
2024-01-07 18:18:24 +0100

    register in progress

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::get('posts', [PostController::class, 'index'])
         ->name('posts.index');
+
+
+Route::post('/register', [RegisteredUserController::class, 'store']);
--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -1,24 +1,47 @@
-import Link from 'next/link'
+"use client";
+
+import Link from 'next/link';
+import { FormEvent } from 'react';
 
 export default function Page() {
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget)
+    const response = await fetch('http://localhost:7717/api/register', {
+      method: 'POST',
+      body: formData,
+    });
+
+    console.log(response)
+
+  };
 
   return (
     <div className="container px-8 mx-auto xl:px-5  max-w-screen-lg py-5 lg:py-8">
       <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
         Register
       </h1>
-      <form className="my-10">
+      <form className="my-10" onSubmit={handleSubmit}>
         <div className="grid my-10 md:grid-cols-2 gap-4">
         <div className="mb-5">
               <input
-                  id="email_address"
+                  placeholder="Name"
+                  className="w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900 focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                  type="text"
+                  name="name"
+              />
+          </div>
+        <div className="mb-5">
+              <input
                   placeholder="Email Address"
                   className="w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900 focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
                   type="email"
                   name="email"
               />
           </div>
-          <div className="mb-5">
+          <div className="col-span-2 mb-5">
               <input
                   placeholder="Password"
                   className="w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900 focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
