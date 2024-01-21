578f459ec2886341304d4a5e97d63f8b940feade
Dmytro Tus <dmytrotus@gmail.com>
2024-01-21 20:42:03 +0100

    disabled strict mode

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx'
@@ -7,11 +6,9 @@ import store from './store/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
 )
