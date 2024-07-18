9d2acdfd71984783571d48cf11426d690682363e
Dmytro Tus <dmytrotus@gmail.com>
2024-07-18 20:35:08 +0200

    Added redis and cached page with 50 posts

--- backend/app/Services/PostService.php
+++ backend/app/Services/PostService.php
@@ -5,11 +5,14 @@ namespace App\Services;
 use Illuminate\Support\Facades\Http;
 use App\Models\Post;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Cache;
 
 class PostService
 {
     public function getPosts(): LengthAwarePaginator
     {
-        return Post::orderBy('id', 'DESC')->paginate(50);
+        return Cache::remember("posts.all.firstpage", $seconds = 60, function () {
+            return Post::orderBy('id', 'DESC')->paginate(50);
+        });
     }
 }
